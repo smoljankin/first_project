425cdd68f13c91d4848decb5256eca37031f8463
smoljankin <smoljankin@gmail.com>
2022-08-14 02:04:13 +0300

    array filling

--- Fistr_project/Fistr_project/Source.cpp
+++ Fistr_project/Fistr_project/Source.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include<time.h>
 
 using namespace std;
 
+#define SIZE 10
+
 int main()
 {
+	srand(time(0));
 
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 100;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 }
